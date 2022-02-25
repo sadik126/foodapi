76c66c88e4da84809d88e9c5068948a227f9f10e
sadik126 <sadikhimel04@gmail.com>
2022-02-25 06:19:08 +0600

    m

--- food.js
+++ food.js
@@ -2,8 +2,45 @@ const food = () =>{
     let searchfood = document.getElementById('searchfood');
     let getfood = searchfood.value;
     console.log(getfood);
+    searchfood.value = '';
     let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${getfood}`
     fetch( url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayfood(data.meals))
+}
+
+
+const displayfood = meals => {
+    console.log(meals);
+    let searchresult = document.getElementById('searchresult');
+    meals.forEach(meal => {
+        console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML =
+        `
+        <div onclick="loadmeal(${meal.idMeal})" class="card">
+                    <img src="${meal.strMealThumb}" width="750" height="300" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${meal.strMeal}</h5>
+                        <p class="card-text">${meal.strInstructions.slice(0,250)}...</p>
+                    </div>
+                </div>
+        `
+    
+        searchresult.appendChild(div);
+    })
+
+  
+    // for (const meal of meals){
+    //     console.log(meal);
+    // }
+}
+
+
+const loadmeal= detail => {
+    let url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${detail}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data.meals[0]))
 }
